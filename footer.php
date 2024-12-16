3983d7a3b822270ee465dce2c4267dba80b74c90
lunaqueen <88500788+lunaqueen@users.noreply.github.com>
2024-12-16 19:33:12 +0800

    Update footer.php

--- footer.php
+++ footer.php
@@ -1,8 +1,6 @@
-<footer>
-    <div class="footer-content">
-        <p>&copy; 2024-<?php echo date('Y'); ?> 版权所有 | Luna</p>
-    </div>
-</footer>
-<?php wp_footer(); ?>
+    <footer>
+        <p>&copy; 1999-<?php echo date("Y"); ?> <?php bloginfo('name'); ?>. 保留所有权利。</p>
+    </footer>
+    <?php wp_footer(); ?>
 </body>
 </html>
